60d0c46b2db1c7fe195f808fa1bc36d685f25302
Ulukbek <ulukbeksulajmanov1@gmail.com>
2025-04-18 15:17:37 +0600

    coinAcceptor теперь реализует paymentAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,34 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
 
-    public CoinAcceptor(int amount) {
-        this.amount = amount;
+    public CoinAcceptor(int initialAmount) {
+        this.amount = initialAmount;
     }
 
+    public CoinAcceptor() {
+        this.amount = 0;
+    }
+
+    @Override
+    public void acceptPayment() {
+        amount += 10;
+        System.out.println("Вы пополнили баланс на 10 монет.");
+    }
+
+    @Override
     public int getAmount() {
         return amount;
     }
 
+    @Override
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public void reset() {
+        this.amount = 0;
     }
 }
